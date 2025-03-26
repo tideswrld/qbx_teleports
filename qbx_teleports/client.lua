29efb300ffca1cca3f83d04a9fcb531aeb5fd2a9
tideswrld <tideswrld@gmail.com>
2025-03-25 17:23:02 -0700

    Update client.lua

--- qbx_teleports/client.lua
+++ qbx_teleports/client.lua
@@ -51,12 +51,35 @@ local function teleportPlayer(destination)
     end
 end
 
+-- Function to check if the player has the required item using ox_inventory
+local function playerHasItem(itemName)
+    return exports.ox_inventory:Search('count', itemName) > 0  -- Using ox_inventory search to check if item is present
+end
+
+-- Function to notify the player when they don't have the required item
+local function notifyPlayer(message)
+    exports.qbx_core:Notify({
+        text = message,           -- The main message
+        notifyType = 'error',     -- The notification type (error in this case)
+        duration = 5000,          -- The duration the notification will remain on screen
+        notifyPosition = 'top-right', -- Position of the notification
+        notifyIcon = 'fas fa-exclamation-circle', -- Icon for the notification
+        notifyIconColor = 'red'  -- Color of the icon (red for error)
+    })
+end
+
 CreateThread(function ()
     for _, passage in ipairs(config.teleports) do
         for i = 1, #passage do
             local entrance = passage[i]
             local exit = passage[(i % 2) + 1]
             local coords = vec3(entrance.coords)
+            
+            -- Only show the target if item is required and player has it, or if itemVisibility is set to true
+            if entrance.itemRequired and not playerHasItem(entrance.itemRequired) and not entrance.itemVisibility then
+                -- Don't add target if the player doesn't have the required item
+                goto continue
+            end
             
             exports.ox_target:addSphereZone({
                 coords = coords,
@@ -67,6 +90,12 @@ CreateThread(function ()
                         label = entrance.label or 'Use Passage',
                         icon = entrance.icon or 'fas fa-door-open',
                         onSelect = function()
+                            -- Notify the player with a general message if they don't have the required item
+                            if entrance.itemRequired and not playerHasItem(entrance.itemRequired) then
+                                notifyPlayer("You're missing something...")
+                                return
+                            end
+                            
                             teleportPlayer({
                                 coords = vec(exit.coords),
                                 ignoreGround = exit.ignoreGround,
@@ -76,6 +105,8 @@ CreateThread(function ()
                     }
                 }
             })
+
+            ::continue::
         end
     end
 end)
